01bd82863c659ac63ffbbb712ddb051f038be281
Wesley Santos <wesley.brazil@outlook.com>
2024-07-08 11:14:25 -0300

    feature: session, interface adicionada

---
+++ types/session.type.ts
@@ -0,0 +1,17 @@
+import { Session, SessionType } from "../entities/Session";
+
+export interface ISessionRepository {
+  create(data: Omit<Session, "id" | "updated_at">): Promise<Session>,
+  findAllSessionByUserId(user_id: string): Promise<Array<Session>>,
+  findById(id: string): Promise<Session>,
+  findByUserId(user_id: string): Promise<Session>,
+  findLastActiveSessionOfUserId(user_id: string): Promise<Session>,
+  update(id: string, data: Partial<SessionType>): Promise<Session>,
+  delete(id: string): Promise<void>,
+}
+
+export interface ISessionSingletonRepository {
+  create(): void,
+  find(): void,
+  remove(): void,
+}
