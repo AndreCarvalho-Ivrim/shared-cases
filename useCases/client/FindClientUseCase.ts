17d6924e15860efdd24d69c8ec649943bb4ab536
Mateus Brandão <mateusfleria@gmail.com>
2024-07-29 11:20:15 -0300

    refector: Traduzindo mensagem de erro do FindClientUseCase

--- useCases/client/FindClientUseCase.ts
+++ useCases/client/FindClientUseCase.ts
@@ -1,23 +1,21 @@
 import { IShortClientRepository } from "../../types/client.type";
 
 export class FindClientUseCase {
-    constructor(private repository: IShortClientRepository){}
+  constructor(private repository: IShortClientRepository){}
 
-    async execute(id: string){
-        const alreadyExists = await this.repository.findById(id);
+  async execute(id: string){
+    const alreadyExists = await this.repository.findById(id);
 
-        if (!alreadyExists){
-            throw new Error("Client does not exist");
-        }
+    if (!alreadyExists) throw new Error("Cliente não encontrado");
 
-        alreadyExists.users = alreadyExists.users.map((user) => {
-            user.userCategory = (user as any).userCategories.find(
-                (category) => category.userCategory.clientId === alreadyExists.id
-            )?.userCategory;
-            delete user.userCategories;
-            return user;
-        });
+    alreadyExists.users = alreadyExists.users.map((user) => {
+      user.userCategory = (user as any).userCategories.find(
+        (category) => category.userCategory.clientId === alreadyExists.id
+      )?.userCategory;
+      delete user.userCategories;
+      return user;
+    });
 
-        return alreadyExists;
-    }
+    return alreadyExists;
+  }
 }
